f8e2f65bd8bf6fd930291a063827d90ccf262361
faizakhirad840 <pc14779.ammar@gmail.com>
2024-09-19 21:20:36 -0700

    Adding milestone_01

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-zinc-800 h-[100vh] text-center p-44 text-white">
+      <h1 className="text-3xl font-bold m-4">Hello World</h1>
+      <p className="text-2xl font-semibold">I am Faiza</p>
+      <p className="text-2xl font-semibold">This is a nextjs project</p>
+    </div>
+  );
+}
